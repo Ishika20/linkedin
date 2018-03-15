0d62f0ccd7de8075ee6b57db64ccb8089f10a631
Ishika20 <31150548+Ishika20@users.noreply.github.com>
2018-03-15 21:49:49 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,16 +1,15 @@
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  $(".g-signin2").css("display","none");
+  $(".data").css("display"."block");
+  $("#pic").attr('src',profile.getImageUrl());
+	$("#email").text(profile.getEmail());
 }
-<a href="#" onclick="signOut();">Sign out</a>
-<script>
   function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-      console.log('User signed out.');
+      alert("You have been successfully signed out");
+      $(".g-signin2").css("display","block");
+      $(".data").css("display"."none");
     });
   }
-</script>
