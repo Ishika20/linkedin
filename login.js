41ed26ad1235e5dca4e96c7044cd1c9c7e22560d
Ishika20 <31150548+Ishika20@users.noreply.github.com>
2018-03-16 13:38:14 +0530

    Update login.js

--- login.js
+++ login.js
@@ -5,11 +5,12 @@ function onSignIn(googleUser) {
   $("#pic").attr('src',profile.getImageUrl());
 	$("#email").text(profile.getEmail());
 }
-  function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      alert("You have been successfully signed out");
-      $(".g-signin2").css("display","block");
-      $(".data").css("visibility","hidden");
-    });
-  }
+  function signOut(){
+		var auth2 = gapi.auth2.getAuthInstance();
+		auth2.signOut().then(function(){
+			alert("You have been Logged Out ");
+			$(".g-signin2").css("display","block");
+			$(".data").css("display","none");
+	       console.log('User signed out.');
+});
+}
